961363b9f8bd8c6fa432ac5bb18ab00ca6882d2b
KenOkvip <151103936+kientayto@users.noreply.github.com>
2024-11-06 10:47:15 +0700

    [Update] transaction for user

--- components/Pages/Home/SectionTwo/Reward/Reward.tsx
+++ components/Pages/Home/SectionTwo/Reward/Reward.tsx
@@ -1,11 +1,14 @@
 import ClaimReward from '@/components/Pages/Home/SectionTwo/Reward/ClaimReward';
 import TransactionReward from '@/components/Pages/Home/SectionTwo/Reward/TransactionReward';
+import { useAuth } from '@/hooks/useAuth';
 
 export const Reward = () => {
+  const { isValidSession } = useAuth();
+
   return (
     <div className="bg-[#1F1F1F] p-4 laptop:p-8 rounded-2xl flex flex-col gap-6 laptop:gap-10 justify-between items-start">
       <ClaimReward />
-      <TransactionReward />
+      {isValidSession && <TransactionReward />}
     </div>
   );
 };
