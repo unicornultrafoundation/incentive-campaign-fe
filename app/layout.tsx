f488bd01ef1f964efdbf72265eac8eda4abe8cb0
vulh <vulh@uniultra.xyz>
2024-11-08 16:11:15 +0700

    [UPDATE]: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,76 @@ import React from 'react';
 import './globals.scss';
 
 import AppProviders from '@/components/Providers';
+import { CAMPAIGN_TYPE } from '@/config/env';
+
+const publicKeywords = [
+  'staking',
+  'yield',
+  'U2U',
+  'mainnet',
+  'campaigns',
+  'coinlist',
+  'incentives',
+  'reward',
+];
+const bitgetKeywords = [
+  'staking',
+  'yield',
+  'U2U',
+  'mainnet',
+  'campaigns',
+  'bitget',
+  'incentives',
+  'reward',
+];
 
 export const metadata: Metadata = {
-  title: 'U2U Liquid',
-  description: '',
+  title:
+    CAMPAIGN_TYPE === 'public'
+      ? 'U2U Incentivized Mainnet Saga'
+      : 'U2U x Bitget Incentivized Campaigns',
+  description:
+    CAMPAIGN_TYPE === 'public'
+      ? 'Staking pUSDT and earn attractive yield on U2U token'
+      : 'Staking pUSDT and earn attractive yield on U2U token',
+  keywords: CAMPAIGN_TYPE === 'public' ? publicKeywords : bitgetKeywords,
+  openGraph: {
+    images:
+      CAMPAIGN_TYPE === 'public'
+        ? {
+            url: 'images/thumbnail/Coinlist.png',
+            width: 1200,
+            height: 600,
+          }
+        : {
+            url: 'images/thumbnail/Bitget.png',
+            width: 1200,
+            height: 600,
+          },
+  },
+  twitter: {
+    images:
+      CAMPAIGN_TYPE === 'public'
+        ? {
+            url: 'images/thumbnail/Coinlist.png',
+            width: 1200,
+            height: 600,
+          }
+        : {
+            url: 'images/thumbnail/Bitget.png',
+            width: 1200,
+            height: 600,
+          },
+    card: 'summary_large_image',
+    title:
+      CAMPAIGN_TYPE === 'public'
+        ? 'U2U Incentivized Mainnet Saga'
+        : 'U2U x Bitget Incentivized Campaigns',
+    description:
+      CAMPAIGN_TYPE === 'public'
+        ? 'Staking pUSDT and earn attractive yield on U2U token'
+        : 'Staking pUSDT and earn attractive yield on U2U token',
+  },
 };
 
 const RootLayout = async ({
@@ -21,34 +87,6 @@ const RootLayout = async ({
 
   return (
     <html lang={locale}>
-      <head>
-        <title>Liquid Staking</title>
-
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="U2U Network DePIN Node Sale" />
-        <meta
-          property="og:description"
-          content="The World’s First DePIN Subnet Node for Decentralized Infrastructure Growth"
-        />
-        <meta property="og:url" content="https://node.depinsubnet.com" />
-        <meta property="og:image" content="/images/og-image.jpeg" />
-        <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="600" />
-
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="U2U Network DePIN Node Sale" />
-        <meta
-          name="twitter:description"
-          content="The World’s First DePIN Subnet Node for Decentralized Infrastructure Growth"
-        />
-        <meta name="twitter:image" content="/images/og-image.jpeg" />
-        <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="600" />
-      </head>
-
       <body>
         {/* <noscript>
           <iframe
