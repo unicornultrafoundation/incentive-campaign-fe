311851cb6b7405e4ff8b40b52e25e1fd512e5cbf
KenOkvip <kenit@okvip.com>
2024-11-08 17:45:03 +0700

    [UPDATE] estimate reward

--- components/Modal/Toast.tsx
+++ components/Modal/Toast.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Modal from '@/components/Modal';
 import Icon from '@/components/Icon';
@@ -10,6 +10,16 @@ export default function ToastModal() {
     toast: { visible, type, title, message, action, actionName },
     clearToast,
   } = useUIStore();
+
+  useEffect(() => {
+    if (visible) {
+      const timer = setTimeout(() => {
+        clearToast();
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [visible, clearToast]);
+
   return (
     <Modal
       className="toast w-[350px] tablet:w-[499px]"
@@ -27,12 +37,6 @@ export default function ToastModal() {
     >
       <div className="content mt-6">
         <hr className="w-full border-[#1F1F1F] border-solid" />
-        {/*{type === 'error' && (*/}
-        {/*  <Icon.Close className={classNames('icon', type)} />*/}
-        {/*)}*/}
-        {/*{type === 'success' && (*/}
-        {/*  <Icon.Check className={classNames('icon', type)} />*/}
-        {/*)}*/}
         <p className="title">{message}</p>
         <div className="relative w-full">
           {type === 'error' ? (
@@ -40,18 +44,6 @@ export default function ToastModal() {
           ) : (
             <Icon.ToastSuccess className="mx-auto" />
           )}
-          {/*<video*/}
-          {/*  className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"*/}
-          {/*  autoPlay*/}
-          {/*  loop*/}
-          {/*  muted*/}
-          {/*  playsInline*/}
-          {/*>*/}
-          {/*  <source*/}
-          {/*    type="video/mp4"*/}
-          {/*    src={type === 'error' ? GifFail : GifSuccess}*/}
-          {/*  />*/}
-          {/*</video>*/}
         </div>
         <p className="text-xs text-[#929292]">
           This will auto-close in 5 seconds. Thank you!
